3c073cd5b9340d5fad9e68a1d4df44209b8e2c01
explosiveseccc <98961241+explosiveseccc@users.noreply.github.com>
2023-04-03 12:43:01 -0400

    5.1 completed

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targetsCatalogue;
+    private float spawnRate = 1f;
+    private float elaspedTime = 0f;
+
+
+
+    void Start(){}
+
+
+    void Update()
+    {
+        elaspedTime += Time.deltaTime;
+        if(elaspedTime > spawnRate){
+            SpawnNewCrate();
+            elapsedTime = 0;
+        }
+    }
+
+    void SpawnNewCrate(){
+        int index = Random.Range(0, targetsCatalogue.Count);
+        Instantiate(targetsCatalogue[index]);
+    }
+}
---
+++ Assets/Scripts/Target.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    private float minSpeed = 112;
+    private float maxSpeed = 116;
+    private float maxTorque = 110;
+    private float xRange = 14;
+    private float ySpawnPos = -12;
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(0, maxSpeed);
+    }
+
+    float RandomTorque()
+    {
+        return Random.Range(0, maxTorque);
+    }
+
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, 1), ySpawnPos);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        rb.AddForce(RandomTorque(), ForceMode.Impulse);
+        rb.AddTorque(RandomTorque(), 0, RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos()
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    private void OnMouseDown ()
+    {
+        Destroy(gameObject);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+    }
+}
